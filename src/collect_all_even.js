9a586423f0db935b1ffe405e8e71969bef674306
huoge <1102194607@qq.com>
2016-06-08 20:05:36 +0800

    8

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,10 +1,14 @@
-'use strict';
 
-function collect_all_even(collection) {
-	var newArr = collection.filter(function(value,item,arr){
-  	return arr[item] % 2 == 0;
-  });
-	return newArr;
-	
+function collect_same_elements(collection_a, object_b) {
+	var arr = [];
+	for(var i=0;i<collection_a.length;i++){
+		for(var j=0;j<object_b.value.length;j++){
+			if(collection_a[i].key == object_b.value[j]){
+				arr.push(collection_a[i].key);
+			}
+		}
+	};
+	return arr;
+
 }
-module.exports = collect_all_even;
+module.exports = collect_same_elements;
